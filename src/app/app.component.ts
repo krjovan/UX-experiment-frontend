3b5533886aeff83444f795599a708d7727d9a419
krjovan <jovankron@gmail.com>
2021-01-19 23:23:23 +0100

    Workflow of experiment created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,57 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'UX-experiment-frontend';
+
+  prviPdf = 'http://localhost:8080/pdfs/sample';
+  drugiPdf = 'http://localhost:8080/pdfs/sample2';
+
+  //redosled kojim ce se izvrsavati testovi, uzima vrednost 1 ili 2
+  //ako je redosled 1 onda se izvrsava prvi pa drugi layout, a ako
+  //je redosled 2 onda se izvrsava drugi pa prvi layout
+  redosled = Math.floor(Math.random() * 2) + 1;
+
+  showIntro = true;
+  showFirstLayout = false;
+  showSecondLayout = false;
+  showThanks = false;
+
+  jmbg: string = '';
+  ime: string = '';
+  prezime: string = '';
+
+  zapocniEksperiment() {
+    this.showIntro = false;
+    if(this.redosled === 1) {
+      window.open(this.prviPdf, "_blank");
+      this.showFirstLayout = true;
+    } else if(this.redosled ===2) {
+      window.open(this.drugiPdf, "_blank");
+      this.showSecondLayout = true;
+    } else {
+      console.log("Nesto neocekivano se dogodilo.");
+    }
+
+  }
+
+  predajPrviLayout() {
+    if(this.redosled === 2) {
+      this.showFirstLayout = false;
+      this.showThanks = true;
+    } else {
+      this.showFirstLayout = false;
+      window.open(this.drugiPdf, "_blank");
+      this.showSecondLayout = true;
+    }
+  }
+
+  predajDrugiLayout() {
+    if(this.redosled === 1) {
+      this.showSecondLayout = false;
+      this.showThanks = true;
+    } else {
+      this.showSecondLayout = false;
+      window.open(this.prviPdf, "_blank");
+      this.showFirstLayout = true;
+    }
+  }
 }
