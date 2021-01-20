eac28357ae4fdcbe0e632a7bd939d829cadd6c68
krjovan <jovankron@gmail.com>
2021-01-20 23:18:38 +0100

    Added timetracking for experiment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,8 +9,8 @@ import { FormGroup } from '@angular/forms';
 export class AppComponent {
   title = 'UX-experiment-frontend';
 
-  prviPdf = 'http://localhost:8080/pdfs/sample';
-  drugiPdf = 'http://localhost:8080/pdfs/sample2';
+  prviPdf = 'http://localhost:8080/pdfs/f';
+  drugiPdf = 'http://localhost:8080/pdfs/s';
 
   //redosled kojim ce se izvrsavati testovi, uzima vrednost 1 ili 2
   //ako je redosled 1 onda se izvrsava prvi pa drugi layout, a ako
@@ -29,14 +29,22 @@ export class AppComponent {
   firstLayoutWindow;
   secondLayoutWindow;
 
+  firstStartTime;
+  firstEndTime;
+
+  secondStartTime;
+  secondEndTime;
+
   zapocniEksperiment() {
     this.showIntro = false;
     if(this.redosled === 1) {
       this.firstLayoutWindow = window.open(this.prviPdf, "_blank");
       this.showFirstLayout = true;
+      this.firstStartTime = performance.now()
     } else if(this.redosled ===2) {
       this.secondLayoutWindow = window.open(this.drugiPdf, "_blank");
       this.showSecondLayout = true;
+      this.secondStartTime = performance.now();
     } else {
       console.log("Nesto neocekivano se dogodilo.");
     }
@@ -45,27 +53,37 @@ export class AppComponent {
 
   predajPrviLayout() {
     if(this.redosled === 2) {
+      this.firstEndTime = performance.now();
+      console.log(Math.round((this.firstEndTime - this.firstStartTime)/1000));
       this.showFirstLayout = false;
       this.firstLayoutWindow.close();
       this.showThanks = true;
     } else {
+      this.firstEndTime = performance.now();
+      console.log(Math.round((this.firstEndTime - this.firstStartTime)/1000));
       this.showFirstLayout = false;
       this.firstLayoutWindow.close();
       this.secondLayoutWindow = window.open(this.drugiPdf, "_blank");
       this.showSecondLayout = true;
+      this.secondStartTime = performance.now();
     }
   }
 
   predajDrugiLayout() {
     if(this.redosled === 1) {
+      this.secondEndTime = performance.now();
+      console.log(Math.round((this.secondEndTime - this.secondStartTime)/1000));
       this.showSecondLayout = false;
       this.secondLayoutWindow.close();
       this.showThanks = true;
     } else {
+      this.secondEndTime = performance.now();
+      console.log(Math.round((this.secondEndTime - this.secondStartTime)/1000));
       this.showSecondLayout = false;
       this.secondLayoutWindow.close();
       this.firstLayoutWindow = window.open(this.prviPdf, "_blank");
       this.showFirstLayout = true;
+      this.firstStartTime = performance.now()
     }
   }
 }
